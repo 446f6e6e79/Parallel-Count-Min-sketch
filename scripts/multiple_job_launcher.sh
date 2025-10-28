7d44f2ddb2fcfce45cb19486adf5b391db4b485f
446f6e6e79 <davidedona03@gmail.com>
2025-10-28 22:09:26 +0100

    Fixed multiple_job_launcher

--- scripts/multiple_job_launcher.sh
+++ scripts/multiple_job_launcher.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 # Setup common parameters for the multiple job submission
+BASE_DIR="Parallel-Count-Min-sketch"
 WALLTIME="01:00:00"
 QUEUE="short_cpuQ"
 MEM="16gb"
 PLACEMENT="pack:excl"
-PARAMETERS="inputName"
-EXECUTABLE="./Parallel-Count-Min-sketch/src"   # Path from the home dir
+PARAMETERS="parameter_for_program"
+EXECUTABLE="${BASE_DIR}/bin/CMsketch"      # Path to the executable file
+TEMPLATE="${BASE_DIR}/scripts/job_template.sh"
+
+# Output directory for generated job scripts
+OUTPUT_DIR="${BASE_DIR}/jobs"
+
 # List of "NODES:NCPUS" combinations to produce
 COMBOS=(
   "1:1"   # 1 cpu, 1 node.       -> P = 1
@@ -17,6 +23,7 @@ COMBOS=(
   "8:4"   # 8 cpu, 4 nodes       -> P = 32
   "8:8"   # 8 cpu, 8 nodes       -> P = 64
 )
+
 # Loop over each combination and generate the corresponding job script
 for combo in "${COMBOS[@]}"; do
   # Split the combo into NODES and NCPUS
@@ -27,15 +34,16 @@ for combo in "${COMBOS[@]}"; do
   # Generate job name dinamically, as job_run_<NODES>n_<NCPUS>c.sh
   out="job_run_${NODES}n_${NCPUS}c.sh"
 
-  sed "s/__NODES__/$NODES/g; \
-       s/__NCPUS__/$NCPUS/g; \
-       s/__MEM__/$MEM/g; \
-       s/__WALLTIME__/$WALLTIME/g; \
-       s/__QUEUE__/$QUEUE/g; \
-       s/__NP__/$NP/g; \
-       s/__EXECUTABLE__/$EXECUTABLE/g; \
-       s/__PARAMETERS__/$PARAMETERS/g; \
-       s/__PLACEMENT__/$PLACEMENT/g" job_template.sh > "$out"
+  sed "s|__EXECUTABLE__|$EXECUTABLE|g; \
+      s|__PLACEMENT__|$PLACEMENT|g; \
+      s|__NODES__|$NODES|g; \
+      s|__NCPUS__|$NCPUS|g; \
+      s|__MEM__|$MEM|g; \
+      s|__WALLTIME__|$WALLTIME|g; \
+      s|__QUEUE__|$QUEUE|g; \
+      s|__NP__|$NP|g; \
+      s|__PARAMETERS__|$PARAMETERS|g" "$TEMPLATE" > "$out"
+
 
   # Make the generated script executable
   chmod +x "$out"
